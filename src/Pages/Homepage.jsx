73be60cc3da54ccda06b809394cc2885dbd5d108
Rishabh-Truwix <rishabhr@truwix.com>
2025-02-27 13:03:12 +0530

    header done, hero section done and television collections done

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import HeroSection from '../Components/HomepageComponents/HeroSection'
+import TelevisionCollections from '../Components/HomepageComponents/TelevisionCollections'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div className='w-full min-h-screen flex flex-col'>
+      <HeroSection />
+      <TelevisionCollections />
+    </div>
   )
 }
 
